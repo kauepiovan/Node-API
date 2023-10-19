7099e733505cc9b958949474dffe1e8c9eb2bdba
Kaue Piovan <kaupiovan@gmail.com>
2023-10-19 12:22:49 -0300

    API node com postgres

---
+++ create-table.js
@@ -0,0 +1,10 @@
+import { sql } from './db.js'
+
+sql`
+  CREATE TABLE videos (
+    id          TEXT PRIMARY KEY,
+    title       TEXT,
+    description TEXT,
+    duration    INTEGER
+  );
+`.then(() => {console.log('table criada!')})
